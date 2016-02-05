92798b0559a243d1f97666c5d731bf0805f9ac4a
Gregwar <g.passault@gmail.com>
2016-02-05 15:00:46 +0100

    SVG circles

--- SVGFormat.cpp
+++ SVGFormat.cpp
@@ -22,15 +22,27 @@ std::string SVGFormat::output()
 
 void SVGFormat::beginPolygon(std::string layer, bool isFirst)
 {
+    circles.clear();
     stream << "<path d=\"";
+}
+
+void SVGFormat::appendStroke(std::string layer, bool isFirst)
+{
+    //if (isFirst) {
+        stream << " stroke=\"" << layer << "\" fill=\"none\" stroke-width=\"0.1\" />";
+    //} else {
+    //    stream << " stroke=\"\" fill=\"" << layer << "\" stroke-width=\"0.1\" />";
+    // }
 }
 
 void SVGFormat::endPolygon(std::string layer, bool isFirst)
 {
-    if (isFirst) {
-        stream << "\" stroke=\"" << layer << "\" fill=\"none\" stroke-width=\"0.1\" />";
-    } else {
-        stream << "\" stroke=\"none\" fill=\"" << layer << "\" stroke-width=\"0.1\" />";
+    stream << "\""; // This closes the path tag
+    appendStroke(layer, isFirst);
+
+    for (auto circle : circles) {
+        stream << "<circle cx=\"" << circle.x << "\" cy=\"" << circle.y << "\" r=\"" << circle.r << "\" ";
+        appendStroke(layer, isFirst);
     }
     
     stream << std::endl;
@@ -56,6 +68,20 @@ void SVGFormat::addPoint(double x, double y)
 void SVGFormat::endPath()
 {
     stream << "z " << std::endl;
+}
+
+bool SVGFormat::supportsCircle()
+{
+    return true;
+}
+
+void SVGFormat::addCircle(std::string layer, double x, double y, double r)
+{
+    Circle c;
+    c.x = x;
+    c.y = y;
+    c.r = r;
+    circles.push_back(c);
 }
         
 double SVGFormat::getXRatio()
--- SVGFormat.h
+++ SVGFormat.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <vector>
 #include "Format.h"
 
 class SVGFormat : public Format
@@ -13,10 +14,19 @@ class SVGFormat : public Format
         virtual void beginPath();
         virtual void addPoint(double x, double y);
         virtual void endPath();
+        virtual bool supportsCircle();
+        virtual void addCircle(std::string layer, double x, double y, double r);
         
         virtual double getXRatio();
         virtual double getYRatio();
 
+        struct Circle {
+            double x, y, r;
+        };
+
     protected:
+        std::vector<Circle> circles;
         bool firstPoint;
+
+        void appendStroke(std::string layer, bool isFirst);
 };
