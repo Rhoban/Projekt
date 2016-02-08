3cae6c5f0613c799c5d4c27973e7bdbf5a413ce7
Gregwar <g.passault@gmail.com>
2016-02-08 10:47:01 +0100

    Adding DXF headers

--- DXFFormat.cpp
+++ DXFFormat.cpp
@@ -8,19 +8,112 @@ std::string DXFFormat::getBaseLayer()
 std::string DXFFormat::output()
 {
     std::stringstream outstr;
-        
-    outstr << "  0" << std::endl;
+    
+    outstr << "999" << std::endl;
+    outstr << "DXF created from Projekt" << std::endl;
+
+    outstr << "0" << std::endl;
     outstr << "SECTION" << std::endl;
-    outstr << "  2" << std::endl;
-    outstr << "ENTITIES" << std::endl;
-	outstr << "  0" << std::endl;
-	outstr << "LAYER" << std::endl;
-	outstr << "  2" << std::endl;
-	outstr << "part" << std::endl;
-    outstr << stream.str();		
-    outstr << "  0" << std::endl;
+    outstr << "2" << std::endl;
+    outstr << "HEADER" << std::endl;
+    outstr << "9" << std::endl;
+    outstr << "$ACADVER" << std::endl;
+    outstr << "1" << std::endl;
+    outstr << "AC1006" << std::endl;
+    outstr << "9" << std::endl;
+    outstr << "$INSBASE" << std::endl;
+    outstr << "10" << std::endl;
+    outstr << "0.0" << std::endl;
+    outstr << "20" << std::endl;
+    outstr << "0.0" << std::endl;
+    outstr << "30" << std::endl;
+    outstr << "0.0" << std::endl;
+    outstr << "9" << std::endl;
+    outstr << "$EXTMIN" << std::endl;
+    outstr << "10" << std::endl;
+    outstr << xMin << std::endl;
+    outstr << "20" << std::endl;
+    outstr << yMin << std::endl;
+    outstr << "9" << std::endl;
+    outstr << "$EXTMAX" << std::endl;
+    outstr << "10" << std::endl;
+    outstr << xMax << std::endl;
+    outstr << "20" << std::endl;
+    outstr << yMax << std::endl;
+    outstr << "0" << std::endl;
     outstr << "ENDSEC" << std::endl;
-    outstr << "  0" << std::endl;
+
+    outstr << "0" << std::endl;
+    outstr << "SECTION" << std::endl;
+    outstr << "2" << std::endl;
+    outstr << "TABLES" << std::endl;
+    outstr << "0" << std::endl;
+    outstr << "TABLE" << std::endl;
+    outstr << "2" << std::endl;
+    outstr << "LTYPE" << std::endl;
+    outstr << "70" << std::endl;
+    outstr << "1" << std::endl;
+    outstr << "0" << std::endl;
+    outstr << "LTYPE" << std::endl;
+    outstr << "2" << std::endl;
+    outstr << "CONTINUOUS" << std::endl;
+    outstr << "70" << std::endl;
+    outstr << "64" << std::endl;
+    outstr << "3" << std::endl;
+    outstr << "Solid line" << std::endl;
+    outstr << "72" << std::endl;
+    outstr << "65" << std::endl;
+    outstr << "73" << std::endl;
+    outstr << "0" << std::endl;
+    outstr << "40" << std::endl;
+    outstr << "0.000000" << std::endl;
+    outstr << "0" << std::endl;
+    outstr << "ENDTAB" << std::endl;
+
+    outstr << "0" << std::endl;
+    outstr << "TABLE" << std::endl;
+    outstr << "2" << std::endl;
+    outstr << "LAYER" << std::endl;
+    outstr << "70" << std::endl;
+    outstr << "6" << std::endl;
+    outstr << "0" << std::endl;
+    outstr << "LAYER" << std::endl;
+    outstr << "2" << std::endl;
+    outstr << "1" << std::endl;
+    outstr << "70" << std::endl;
+    outstr << "64" << std::endl;
+    outstr << "62" << std::endl;
+    outstr << "7" << std::endl;
+    outstr << "6" << std::endl;
+    outstr << "CONTINUOUS" << std::endl;
+    outstr << "0" << std::endl;
+    outstr << "ENDTAB" << std::endl;
+    outstr << "0" << std::endl;
+    outstr << "TABLE" << std::endl;
+    outstr << "2" << std::endl;
+    outstr << "STYLE" << std::endl;
+    outstr << "70" << std::endl;
+    outstr << "0" << std::endl;
+    outstr << "0" << std::endl;
+    outstr << "ENDTAB" << std::endl;
+    outstr << "0" << std::endl;
+    outstr << "ENDSEC" << std::endl;
+
+    outstr << "0" << std::endl;
+    outstr << "SECTION" << std::endl;
+    outstr << "2" << std::endl;
+    outstr << "BLOCKS" << std::endl;
+    outstr << "0" << std::endl;
+    outstr << "ENDSEC" << std::endl;
+
+    outstr << "0" << std::endl;
+    outstr << "SECTION" << std::endl;
+    outstr << "2" << std::endl;
+    outstr << "ENTITIES" << std::endl;
+    outstr << stream.str();		
+    outstr << "0" << std::endl;
+    outstr << "ENDSEC" << std::endl;
+    outstr << "0" << std::endl;
     outstr << "EOF" << std::endl;
 
     return outstr.str();
@@ -47,19 +140,19 @@ void DXFFormat::addPoint(double x, double y)
         lY = fY = y;
         firstPoint = false;
     } else {
-        stream << "  0" << std::endl;
+        stream << "0" << std::endl;
         stream << "LINE" << std::endl;
-        stream << "  8" << std::endl;
-        stream << "part" << std::endl;
-        stream << "  10" << std::endl;
+        stream << "8" << std::endl;
+        stream << "1" << std::endl;
+        stream << "10" << std::endl;
         stream << lX << std::endl;
-        stream << "  20" << std::endl;
+        stream << "20" << std::endl;
         stream << lY << std::endl;
-        stream << "  11" << std::endl;
+        stream << "11" << std::endl;
         stream << x << std::endl;
-        stream << "  21" << std::endl;
+        stream << "21" << std::endl;
         stream << y << std::endl;
-        stream << "  62" << std::endl;
+        stream << "62" << std::endl;
         stream << currentLayer << std::endl;
 
         lX = x;
@@ -70,19 +163,19 @@ void DXFFormat::addPoint(double x, double y)
 void DXFFormat::endPath()
 {
     if (!firstPoint) {
-        stream << "  0" << std::endl;
+        stream << "0" << std::endl;
         stream << "LINE" << std::endl;
-        stream << "  8" << std::endl;
-        stream << "part" << std::endl;
-        stream << "  10" << std::endl;
+        stream << "8" << std::endl;
+        stream << "1" << std::endl;
+        stream << "10" << std::endl;
         stream << lX << std::endl;
-        stream << "  20" << std::endl;
+        stream << "20" << std::endl;
         stream << lY << std::endl;
-        stream << "  11" << std::endl;
+        stream << "11" << std::endl;
         stream << fX << std::endl;
-        stream << "  21" << std::endl;
+        stream << "21" << std::endl;
         stream << fY << std::endl;
-        stream << "  62" << std::endl;
+        stream << "62" << std::endl;
         stream << currentLayer << std::endl;
     }
 }
@@ -94,14 +187,16 @@ bool DXFFormat::supportsCircle()
 
 void DXFFormat::addCircle(std::string layer, double x, double y, double r)
 {
-    stream << "  0" << std::endl;
+    stream << "0" << std::endl;
     stream << "CIRCLE" << std::endl;
-    stream << "  10" << std::endl;
+    stream << "8" << std::endl;
+    stream << "1" << std::endl;
+    stream << "10" << std::endl;
     stream << x << std::endl;
-    stream << "  20" << std::endl;
+    stream << "20" << std::endl;
     stream << y << std::endl;
-    stream << "  40" << std::endl;
+    stream << "40" << std::endl;
     stream << r << std::endl;
-    stream << "  62" << std::endl;
+    stream << "62" << std::endl;
     stream << layer << std::endl;
 }
