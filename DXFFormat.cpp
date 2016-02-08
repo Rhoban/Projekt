3caaf07da7d7470cc424633dfd990cbcf01c7c22
Gregwar <g.passault@gmail.com>
2016-02-08 10:16:18 +0100

    Adding spaces in dxf

--- DXFFormat.cpp
+++ DXFFormat.cpp
@@ -9,18 +9,18 @@ std::string DXFFormat::output()
 {
     std::stringstream outstr;
         
-    outstr << "0" << std::endl;
+    outstr << "  0" << std::endl;
     outstr << "SECTION" << std::endl;
-    outstr << "2" << std::endl;
+    outstr << "  2" << std::endl;
     outstr << "ENTITIES" << std::endl;
-	outstr << "0" << std::endl;
+	outstr << "  0" << std::endl;
 	outstr << "LAYER" << std::endl;
-	outstr << "2" << std::endl;
+	outstr << "  2" << std::endl;
 	outstr << "part" << std::endl;
     outstr << stream.str();		
-    outstr << "0" << std::endl;
+    outstr << "  0" << std::endl;
     outstr << "ENDSEC" << std::endl;
-    outstr << "0" << std::endl;
+    outstr << "  0" << std::endl;
     outstr << "EOF" << std::endl;
 
     return outstr.str();
@@ -47,19 +47,19 @@ void DXFFormat::addPoint(double x, double y)
         lY = fY = y;
         firstPoint = false;
     } else {
-        stream << "0" << std::endl;
+        stream << "  0" << std::endl;
         stream << "LINE" << std::endl;
-		stream << "8" << std::endl;
-		stream << "part" << std::endl;
-        stream << "10" << std::endl;
+        stream << "  8" << std::endl;
+        stream << "part" << std::endl;
+        stream << "  10" << std::endl;
         stream << lX << std::endl;
-        stream << "20" << std::endl;
+        stream << "  20" << std::endl;
         stream << lY << std::endl;
-        stream << "11" << std::endl;
+        stream << "  11" << std::endl;
         stream << x << std::endl;
-        stream << "21" << std::endl;
+        stream << "  21" << std::endl;
         stream << y << std::endl;
-        stream << "62" << std::endl;
+        stream << "  62" << std::endl;
         stream << currentLayer << std::endl;
 
         lX = x;
@@ -70,19 +70,19 @@ void DXFFormat::addPoint(double x, double y)
 void DXFFormat::endPath()
 {
     if (!firstPoint) {
-        stream << "0" << std::endl;
+        stream << "  0" << std::endl;
         stream << "LINE" << std::endl;
-		stream << "8" << std::endl;
-		stream << "part" << std::endl;
-        stream << "10" << std::endl;
+        stream << "  8" << std::endl;
+        stream << "part" << std::endl;
+        stream << "  10" << std::endl;
         stream << lX << std::endl;
-        stream << "20" << std::endl;
+        stream << "  20" << std::endl;
         stream << lY << std::endl;
-        stream << "11" << std::endl;
+        stream << "  11" << std::endl;
         stream << fX << std::endl;
-        stream << "21" << std::endl;
+        stream << "  21" << std::endl;
         stream << fY << std::endl;
-        stream << "62" << std::endl;
+        stream << "  62" << std::endl;
         stream << currentLayer << std::endl;
     }
 }
@@ -94,14 +94,14 @@ bool DXFFormat::supportsCircle()
 
 void DXFFormat::addCircle(std::string layer, double x, double y, double r)
 {
-    stream << "0" << std::endl;
+    stream << "  0" << std::endl;
     stream << "CIRCLE" << std::endl;
-    stream << "10" << std::endl;
+    stream << "  10" << std::endl;
     stream << x << std::endl;
-    stream << "20" << std::endl;
+    stream << "  20" << std::endl;
     stream << y << std::endl;
-    stream << "40" << std::endl;
+    stream << "  40" << std::endl;
     stream << r << std::endl;
-    stream << "62" << std::endl;
+    stream << "  62" << std::endl;
     stream << layer << std::endl;
 }
